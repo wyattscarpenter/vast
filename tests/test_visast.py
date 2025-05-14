59d97586f261cc9f7e191cf285b63b631ac41987
wyattscarpenter <wyattscarpenter@gmail.com>
2025-05-13 19:25:37 -0700

    refactor the ASTCompare class to just be a regular function

--- tests/test_visast.py
+++ tests/test_visast.py
@@ -1,6 +1,7 @@
 import unittest
 import ast
 from os import unlink
+from typing import Union
 from visast import generate
 
 class TestVisAST(unittest.TestCase):
@@ -37,14 +38,12 @@ if __name__ == "__main__":
     def testGenerateFromURLTwo(self) -> None:
         aST = ast.parse(self.aSTScript)
         remoteAST = generate.fromURL(self.aSTURL)
-        c = ASTCompare()
-        self.assertTrue(c.compare_ast(aST, remoteAST), "AST from URL should be correct")
+        self.assertTrue(self.compare_ast(aST, remoteAST), "AST from URL should be correct")
 
     def testGenerateFromStringTwo(self) -> None:
         defaultAST = ast.parse(self.aSTScript)
         generatedAST = generate.fromString(self.aSTScript)
-        c = ASTCompare()
-        self.assertTrue(c.compare_ast(defaultAST, generatedAST), "AST from string should be correct")
+        self.assertTrue(self.compare_ast(defaultAST, generatedAST), "AST from string should be correct")
 
     def testGenerateFrompathOne(self) -> None:
         import tempfile
@@ -58,14 +57,11 @@ if __name__ == "__main__":
             unlink(tmp.name)
         self.assertIsInstance(aST, ast.AST, "generate.fromPath should generate an AST")
 
-class ASTCompare:
-    """ Class for comparing ASTs.
-    Copy of Jonathan Biemond's answer to https://stackoverflow.com/questions/76352198/how-to-compare-two-python-asts-ignoring-arguments
-    """
-    from typing import Union
-
     def compare_ast(self, node1: Union[ast.AST, ast.expr, list[ast.expr]], node2: Union[ast.AST, ast.expr, list[ast.expr]], ignore_args: bool =False) -> bool:
-        """Compare two AST nodes for equality."""
+        """
+        Function for comparing ASTs.
+        Copy of Jonathan Biemond's answer to https://stackoverflow.com/questions/76352198/how-to-compare-two-python-asts-ignoring-arguments
+        Compare two AST nodes for equality."""
         from itertools import zip_longest
         if type(node1) is not type(node2):
             return False
